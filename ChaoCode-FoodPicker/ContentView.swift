bfdecc2246f5acfc2476555509e28403b5d5b7e8
Wise Wong <imhwc@icloud.com>
2024-06-03 21:27:37 +0800

    新增 buttonAdjust方法，简化按钮重复代码量； 增加背景padding 和颜色； 再次抽离公共属性

--- ChaoCode-FoodPicker/ContentView.swift
+++ ChaoCode-FoodPicker/ContentView.swift
@@ -12,52 +12,48 @@ struct ContentView: View {
     let foods = ["冻干","猫粮","猫条","水","没得吃","鸡蛋黄","牛肉","三文鱼","秋刀鱼","鸡胸肉","蓝莓","草莓","苹果","燕麦","豆腐","胡萝卜","玉米",]
     
     @State private var randomFood :String?
+    
     var body: some View {
         VStack (alignment:.center){
             Image("heliotrope")
                 .resizable()
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
-                .padding()
-            
+                .aspectRatio(contentMode: .fit)
             Text(randomFood ?? "小葵今天吃什莫❓❓")
                 .font(.largeTitle)
-                .bold()
                 .kerning(2.0)
             buttonView
         }
+        .frame(maxWidth: .infinity,maxHeight: .infinity)
+        .background(Color(.secondarySystemBackground))
+        .bold()
         .padding()
         .animation(.easeInOut, value: randomFood)
     }
     
     var buttonView: some View{
-            VStack{
-                Button(action: {
-                    self.randomFoodSelect()
-                }, label: {
-                    Image(systemName: "person.crop.circle.fill.badge.questionmark")
-                    Text(randomFood == .none ? "问问 11 🐷" : "换一个")
-                        .bold()
-                        .frame(width: 120)
-                })
-                .buttonStyle(.borderedProminent)
-                .buttonBorderShape(.capsule)
-                .controlSize(.large)
-                .font(.title2)
-                .tint(.brown)
-            
-                Button(action: {
-                    randomFood = .none
-                }, label: {
-                    Image(systemName: "arrow.clockwise.circle.fill")
-                    Text("重头来过")
-                        .bold()
-                        .frame(width: 120)
-                })
-                .buttonStyle(.bordered)
-                .buttonBorderShape(.capsule)
-                .controlSize(.large)
-                .font(.title2)
+        VStack{
+            buttonAdjust(by:1,symbol: "person.crop.circle.fill.badge.questionmark" ,txt: randomFood == .none ? "问问 11 🐷" : "换一个")
+            .buttonStyle(.borderedProminent)
+            buttonAdjust(by:0,symbol: "arrow.clockwise.circle.fill" ,txt: "重头来过")
+            .buttonStyle(.bordered)
+        }
+    }
+    
+    func buttonAdjust(by def :Int,symbol:String,txt:String) -> some View{
+        Button(action: {
+            if def == 1{
+                self.randomFoodSelect()
+            }else{
+                randomFood = .none
             }
+            
+        }, label: {
+            Image(systemName: symbol)
+            Text(txt).frame(width:120)
+        })
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        .font(.title2)
     }
     
     func randomFoodSelect(){
